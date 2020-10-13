1efeccd35fbce0a8bfb0ce39b976b4c9596b0428
acoomes33 <github email address>
2020-10-12 19:30:17 -0500

    added model associations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  has_many :ingredient_recipes
+  has_many :comments
+  has_many :ingredients, through: :ingredient_recipes
+  has_many :users, through: :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    has_many :comments
+    has_many :commented_recipes, through: :comments, source: :recipe
+    has_many :recipes
 end
