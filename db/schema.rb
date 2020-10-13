d5980afa6f8402f329e72dc88b7a327469a0a5ca
acoomes33 <github email address>
2020-10-12 19:17:38 -0500

    created comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_13_001421) do
+ActiveRecord::Schema.define(version: 2020_10_13_001649) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "rating"
+    t.integer "user_id", null: false
+    t.integer "recipe_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["recipe_id"], name: "index_comments_on_recipe_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
@@ -40,5 +52,7 @@ ActiveRecord::Schema.define(version: 2020_10_13_001421) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "recipes"
+  add_foreign_key "comments", "users"
   add_foreign_key "recipes", "users"
 end
