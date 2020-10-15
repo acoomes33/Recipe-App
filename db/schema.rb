4dcab3ebd0de288605382db69503e2585fcfb099
acoomes33 <github email address>
2020-10-15 18:57:09 -0500

    deleted ingredient join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_14_233510) do
+ActiveRecord::Schema.define(version: 2020_10_15_233026) do
 
   create_table "comments", force: :cascade do |t|
     t.string "title"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2020_10_14_233510) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["recipe_id"], name: "index_comments_on_recipe_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "dietary_restrictions", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "ingredient_recipes", force: :cascade do |t|
