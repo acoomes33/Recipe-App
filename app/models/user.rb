ab784204c41968daf2cba7fefc421702305e1c23
acoomes33 <github email address>
2020-10-14 20:47:30 -0500

    addd full name method

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
     has_secure_password
 
     validates_presence_of :first_name, :last_name
+
+    def self.full_name 
+        "#{self.first_name} #{self.last_name}"
+    end 
+
 end
