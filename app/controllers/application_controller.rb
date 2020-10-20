a7ef174a1d2d6ccb3324344776de618b7944f2e5
acoomes33 <github email address>
2020-10-20 17:32:04 -0500

    customized authorization error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
     helper_method :current_user
     helper_method :logged_in?
 
+    rescue_from CanCan::AccessDenied do |exception|
+        flash[:notice] = exception.message
+        redirect_to root_path
+    end
+
     def current_user 
         User.find_by(id: session[:user_id]) if logged_in?
     end 
